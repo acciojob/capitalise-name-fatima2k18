8a5b14190f39a0309984e2af42cd3b4d9ad64469
Fatima Ejaz <92662988+fatima2k18@users.noreply.github.com>
2025-06-01 15:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+// Add an event listener for the 'blur' event
+nameInput.addEventListener("blur", function () {
+  // Convert the input value to uppercase when the field loses focus
+  // The blur event fires when the element loses focus (e.g., user clicks away from the input)
+  nameInput.value = nameInput.value.toUpperCase();
